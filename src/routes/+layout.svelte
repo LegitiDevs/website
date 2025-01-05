636bff8e3775df8d07e9296daa18e326cf38746a
Skylar <commit@nobleskye.dev>
2025-01-04 17:29:25 -0800

    fixed changs i think- im not sure what the issue is but i changed spelling and now the preview is saying its btter ig?

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -32,27 +32,28 @@
 {@render children()}
 
 <div class="footer-container">
-	<p>This is not an official Moose project and is made by the community.</p>
-	<p>We have no affiliation with any real-world brands.</p>
-	<p>Not affiliated with Mojang AB or Partners</p>
-	
-	<div class="links-container">
-		<a href="https://store.skyenet.co.in">API Hosted by SkyeNet</a>
-		<a href="https://github.com/LegitiDevs/">
-			<img src="/svg/github-mark-white.svg" alt="Github Logo" />
-		</a>
-		<a href="https://discord.gg/FQUmnbgV5S">
-			<img src="/svg/discord-mark-white.svg" alt="Discord Logo" />
-		</a>
-		<a href="https://store.legitimoose.com">store.legitimoose.com</a>
+    <p>This is not an official Moose project and is made by the community.</p>
+    <p>We have no affiliation with any real-world brands.</p>
+    <p>Not affiliated with Mojang AB or Partners</p>
 
-		<a href="https://youtube.com/legitimoose">
-			<img src="/img/youtube.png" alt="Legitimoose's Youtube" />
-		</a>
-		<a href="https://bsky.app/profile/legitidevs.bsky.social">
-			<img src="/svg/bsky.svg" alt="LegitiDev BlueSky" />
-	</div>
+    <div class="links-container">
+        <a href="https://store.skyenet.co.in">API Hosted by SkyeNet</a>
+        <a href="https://github.com/LegitiDevs/">
+            <img src="/svg/github-mark-white.svg" alt="Github Logo" />
+        </a>
+        <a href="https://discord.gg/FQUmnbgV5S">
+            <img src="/svg/discord-mark-white.svg" alt="Discord Logo" />
+        </a>
+        <a href="https://store.legitimoose.com">store.legitimoose.com</a>
+        <a href="https://youtube.com/legitimoose">
+            <img src="/img/youtube.png" alt="Legitimoose's Youtube" />
+        </a>
+        <a href="https://bsky.app/profile/legitidevs.bsky.social">
+            <img src="/svg/bsky.svg" alt="LegitiDev BlueSky" />
+        </a>
+    </div>
 </div>
+
 
 <style>
 	.navbar {

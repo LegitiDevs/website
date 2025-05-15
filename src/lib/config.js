e012b2e935e62060c5b60473b000aade312a981c
Trplnr <87922281+Trioplane@users.noreply.github.com>
2025-05-15 15:37:57 +0800

    add latest legitimoose version

--- src/lib/config.js
+++ src/lib/config.js
@@ -1,8 +1,9 @@
 export const SITE_CONFIG = {
-	API_ROOT: "https://api.legitimoose.net/", // PROD: "https://api.legitimoose.net"
+	API_ROOT: "https://api.legitimoose.net/", // PROD: "https://api.legitimoose.net/"
 	SITE_ROOT: "https://legitimoose.net/", // PROD: "https://legitimoose.net/"
 	MCAUTH: {
 		CLIENT_ID: "3547577294300120212", // PROD: 3547577294300120212
 		REDIRECT_URI: "https://legitimoose.net/api/profile/login",
 	},
+	LATEST_LEGITIMOOSE_VERSION: "1.21.4",
 };

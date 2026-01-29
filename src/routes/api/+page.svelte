9bf71424e31f6866dead5100ff25c2f959f8cce7
Omri H <omri.hermon@gmail.com>
2026-01-29 22:24:05 +0200

    update documentation

--- src/routes/api/+page.svelte
+++ src/routes/api/+page.svelte
@@ -1,6 +1,6 @@
 <script>
 	import SITE_CONFIG from "$lib/config.json";
-	import { PUBLIC_API_ROOT } from '$env/static/public'
+	import { PUBLIC_API_ROOT } from "$env/static/public";
 	import { codeToHtml } from "shiki";
 
 	let inputValue = $state("");
@@ -43,7 +43,9 @@
 	<div class="line"></div>
 	<div class="section">
 		<h2>
-			<a id="fetching-the-api" href="#fetching-the-api">Fetching the API</a>
+			<a id="fetching-the-api" href="#fetching-the-api"
+				>Fetching the API</a
+			>
 		</h2>
 		<div class="text-block">
 			<p>
@@ -59,57 +61,62 @@
 
 		<div class="text-block">
 			<p>
-				<code>{`/world/<world_uuid>`}</code> retrieves the data for the world with the
-				specified UUID.
+				<code>{`/world/<world_uuid>`}</code> retrieves the data for the world
+				with the specified UUID.
 			</p>
 			<p>
 				<code>{`/world/random`}</code> retrieves the data for a random world
 			</p>
 			<p>
-				<code>{`/page/<index>`}</code> retrieves the data for the 27 worlds in the
-				specified page index. (Imagine a chest with 27 items)
+				<code>{`/page/<index>`}</code> retrieves the data for the 27 worlds
+				in the specified page index. (Imagine a chest with 27 items)
 			</p>
 			<p>
 				<code>{`/top/<number>`}</code> retrieves the data for the top
 				<code>&lt;number&gt;</code> worlds.
 			</p>
 			<p>
-				<code>{`/search/<query>`}</code> retrieves the data for the worlds that their
-				name closely matches the query.
+				<code>{`/search/<query>`}</code> retrieves the data for the worlds
+				that their name closely matches the query.
 			</p>
 			<p>
-				<code>/top/0</code> retrieves the data for all worlds, sorted from the top-ranked
-				to the lowest-ranked.
+				<code>/top/0</code> retrieves the data for all worlds, sorted from
+				the top-ranked to the lowest-ranked.
 			</p>
 			<p>
-				<code>{`/owner/<player_uuid>`}</code> retrieves the data for each world made by the specified owner.
+				<code>{`/owner/<player_uuid>`}</code> retrieves the data for each
+				world made by the specified owner.
 			</p>
 			<p>
-				<code>/all</code> retrieves the data for all worlds, sorted by the most recently
-				updated.
+				<code>/all</code> retrieves the data for all worlds, sorted by the
+				most recently updated.
 			</p>
 		</div>
 
 		<div class="text-block">
 			<p>
 				There are also 2 optional arguments for the <code>/all</code>,
-				<code>{`/search/<query>`}</code>, <code>{`/page/<index>`}</code>, and <code>{`/owner/<player_uuid>`}</code> endpoints.
+				<code>{`/search/<query>`}</code>,
+				<code>{`/page/<index>`}</code>, and
+				<code>{`/owner/<player_uuid>`}</code> endpoints.
 			</p>
-			<br>
+			<br />
 			<p>These are:</p>
 			<p>
-				<code>{`?sort`}</code> - Sorts the world list depending on the value given.
+				<code>{`?sort`}</code> - Sorts the world list depending on the value
+				given.
 			</p>
 			<p>
 				Options:
-				<code>default|votes|visits|recently_scraped|recently_created</code>.
-				Defaults to <code>default</code> (Online worlds, then player count, then
-				votes)
+				<code
+					>default|votes|visits|recently_scraped|recently_created</code
+				>. Defaults to <code>default</code> (Online worlds, then player count,
+				then votes)
 			</p>
 			<br />
 			<p>
-				<code>{`?sortDirection`}</code> - Sorts the direction of the world list depending
-				on the value given.
+				<code>{`?sortDirection`}</code> - Sorts the direction of the world
+				list depending on the value given.
 			</p>
 			<p>
 				Options: <code>ascending|descending</code>.
@@ -156,20 +163,23 @@
 	<div class="line"></div>
 	<div class="section">
 		<h2>
-			<a id="reading-the-data" href="#reading-the-data">Reading the data</a>
+			<a id="reading-the-data" href="#reading-the-data"
+				>Reading the data</a
+			>
 		</h2>
 		<div class="text-block">
 			<p>
-				The data is structured differently depending on what endpoint you
-				called, but the world data will always stay the same structure.
+				The data is structured differently depending on what endpoint
+				you called, but the world data will always stay the same
+				structure.
 			</p>
 		</div>
 
 		<div class="text-block">
 			<p>
-				Calling <code>/world/&lt;uuid&gt;</code> returns a single object containing
-				the world's data, while calling any of the other three will return an array
-				of worlds.
+				Calling <code>/world/&lt;uuid&gt;</code> returns a single object
+				containing the world's data, while calling any of the other three
+				will return an array of worlds.
 			</p>
 		</div>
 
@@ -178,27 +188,29 @@
 			<div class="code-container">
 				<code class="language-js code-block">
 					{#await codeToHtml( `
-	{
-		"world_uuid": "3253a682-3a74-413d-8917-0edd92a792db",
-		"name": "BLOCKS 'N PROPS v2",
-		"description": "Chicken Volleyball out now!",
-		"raw_name": "{\\"text\\":\\"\\",\\"extra\\":[{\\"text\\":\\"B\\",\\"color\\":\\"#00FBFF\\"},{\\"text\\":\\"L\\",\\"color\\":\\"#0CF8FC\\"},{\\"text\\":\\"O\\",\\"color\\":\\"#18F5F9\\"},{\\"text\\":\\"C\\",\\"color\\":\\"#24F3F6\\"},{\\"text\\":\\"K\\",\\"color\\":\\"#30F0F3\\"},{\\"text\\":\\"S\\",\\"color\\":\\"#3CEDF0\\"},{\\"text\\":\\" \\",\\"color\\":\\"#48EAED\\"},{\\"text\\":\\"'\\",\\"color\\":\\"#54E8EA\\"},{\\"text\\":\\"N\\",\\"color\\":\\"#60E5E7\\"},{\\"text\\":\\" \\",\\"color\\":\\"#6CE2E4\\"},{\\"text\\":\\"P\\",\\"color\\":\\"#78DFE1\\"},{\\"text\\":\\"R\\",\\"color\\":\\"#84DDDE\\"},{\\"text\\":\\"O\\",\\"color\\":\\"#90DADB\\"},{\\"text\\":\\"P\\",\\"color\\":\\"#9CD7D8\\"},{\\"text\\":\\"S\\",\\"color\\":\\"#A8D4D5\\"},{\\"text\\":\\" \\",\\"color\\":\\"#B4D2D2\\"},{\\"text\\":\\"v\\",\\"color\\":\\"#C0CFCF\\"},{\\"text\\":\\"2\\",\\"color\\":\\"#CCCCCC\\"}],\\"italic\\":false,\\"bold\\":true}",
-		"raw_description": "{\\"text\\":\\"\\",\\"extra\\":[{\\"text\\":\\"Chicken Volleyball\\",\\"color\\":\\"green\\"},\\" out now!\\"],\\"italic\\":false,\\"color\\":\\"gray\\"}",
-		"icon": "minecraft:recovery_compass",
-		"votes": 353,
-		"visits": 3889,
-		"owner_uuid": "79ffd566-c9db-4159-8bd6-97c18ffd3c66",
-		"player_count": 0,
-		"creation_date": "Dec 24, 2023, 10:26 AM",
-		"creation_date_unix_seconds": "	1703384760",
-		"resource_pack_url": "https://download.mc-packs.net/pack/2e9d8315ec8df8bd81dc5a924deb5ff26b402cb0.zip",
-		"locked": false,
-		"version": "1.21.1",
-		"enforce_whitelist": false,
-		"whitelist_on_version_change": false,
-		"last_scraped": 1734160015,
-	}
-					`, { lang: "json", theme: "dark-plus" } )}
+{
+  "_id": "697aa81b542ad01ead01177e",
+  "creation_date": "Dec 19, 2025, 9:00 PM",
+  "creation_date_unix_seconds": 1766206810,
+  "enforce_whitelist": false,
+  "locked": false,
+  "owner_uuid": "97df29a6-132c-4f04-9d91-79c32a3488cd",
+  "player_count": 0,
+  "resource_pack_url": "https://download.mc-packs.net/pack/ad7686684956c788389d9bfa86ebfa2f0c8fcca6.zip",
+  "world_uuid": "620a3e26-72a8-4e48-a920-370d3490a273",
+  "version": "1.21.10",
+  "visits": 369,
+  "votes": 35,
+  "whitelist_on_version_change": false,
+  "name": "Mass Failure",
+  "description": "floor 2 works fr fr :D",
+  "jam_world": false,
+  "jam_id": 2,
+  "raw_name": "{\"text\":\"\",\"extra\":[{\"text\":\"M\",\"color\":\"#949E00\"},{\"text\":\"a\",\"color\":\"#5E7C00\"},{\"text\":\"s\",\"color\":\"#285A00\"},{\"text\":\"s\",\"color\":\"#0D4900\"},{\"text\":\" \",\"color\":\"#436B00\"},{\"text\":\"Fa\",\"color\":\"#798D00\"},{\"text\":\"i\",\"color\":\"#436B00\"},{\"text\":\"l\",\"color\":\"#0D4900\"},{\"text\":\"u\",\"color\":\"#285A00\"},{\"text\":\"r\",\"color\":\"#5E7C00\"},{\"text\":\"e\",\"color\":\"#949E00\"}],\"bold\":true,\"italic\":false,\"color\":\"white\"}",
+  "raw_description": "{\"text\":\"floor 2 works fr fr :D\",\"italic\":false,\"color\":\"yellow\"}",
+  "icon": "minecraft:skeleton_skull",
+  "last_scraped": 1769717298
+}`, { lang: "json", theme: "dark-plus" }, )}
 						Higlighting syntax...
 					{:then code}
 						{@html code}
@@ -207,7 +219,9 @@
 			</div>
 			<ul>
 				<li><p><code>"world_uuid"</code> - The world's UUID.</p></li>
-				<li><p><code>"name"</code> - The world's name in plaintext.</p></li>
+				<li>
+					<p><code>"name"</code> - The world's name in plaintext.</p>
+				</li>
 				<li>
 					<p>
 						<code>"description"</code> - The world's description in plaintext.
@@ -215,78 +229,96 @@
 				</li>
 				<li>
 					<p>
-						<code>"raw_name"</code> - The world's name in Raw JSON Text Component
-						format, preserving styling.
+						<code>"raw_name"</code> - The world's name in Raw JSON Text
+						Component format, preserving styling.
 					</p>
 				</li>
 				<li>
 					<p>
-						<code>"raw_description"</code> - The world's description in Raw JSON
-						Text Component format, preserving styling.
+						<code>"raw_description"</code> - The world's description
+						in Raw JSON Text Component format, preserving styling.
 					</p>
 				</li>
 				<li>
 					<p>
-						<code>"icon"</code> - The world's item icon in item ID form, including
-						the namespace.
+						<code>"icon"</code> - The world's item icon in item ID form,
+						including the namespace.
 					</p>
 				</li>
 				<li><p><code>"votes"</code> - The world's vote count.</p></li>
 				<li><p><code>"visits"</code> - The world's visit count.</p></li>
 				<li>
 					<p>
-						<code>"player_count"</code> - The current player count in the world.
-					</p>
-				</li>
-				<li>
-					<p><code>"owner_uuid"</code> - The UUID of the world's owner.</p>
-				</li>
-				<li>
-					<p>
-						<code>"creation_date"</code> - The creation date of the world as a string
-						(PST timezone)
+						<code>"player_count"</code> - The current player count in
+						the world.
 					</p>
 				</li>
 				<li>
 					<p>
-						<code>"creation_date_unix_seconds"</code> - The creation date of the
-						world in UNIX seconds
+						<code>"owner_uuid"</code> - The UUID of the world's owner.
 					</p>
 				</li>
 				<li>
 					<p>
-						<code>"resource_pack_url"</code> - The download URL for the world's
-						resource pack from <code>https://mc-packs.net/</code>. An empty
-						string if the world has no resource pack.
+						<code>"creation_date"</code> - The creation date of the world
+						as a string (PST timezone)
 					</p>
 				</li>
 				<li>
 					<p>
-						<code>"version"</code> - The world's version (for warning purposes only;
-						does not affect the actual version).
+						<code>"creation_date_unix_seconds"</code> - The creation
+						date of the world in UNIX seconds
 					</p>
 				</li>
 				<li>
 					<p>
-						<code>"locked"</code> - Indicates whether the world is online and running.
+						<code>"resource_pack_url"</code> - The download URL for
+						the world's resource pack from
+						<code>https://mc-packs.net/</code>. An empty string if
+						the world has no resource pack.
 					</p>
 				</li>
 				<li>
 					<p>
-						<code>"enforce_whitelist"</code> - Indicates whether the world has a
-						whitelist enforced.
+						<code>"version"</code> - The world's version (for warning
+						purposes only; does not affect the actual version).
 					</p>
 				</li>
 				<li>
 					<p>
-						<code>"whitelist_on_version_change"</code> - Indicates whether the whitelist-on-version-change
-						setting is enabled.
+						<code>"locked"</code> - Indicates whether the world is online
+						and running.
 					</p>
 				</li>
 				<li>
 					<p>
-						<code>"last_scraped"</code> - The unix timestamp of the world's last
-						scrape by our bot.
+						<code>"enforce_whitelist"</code> - Indicates whether the
+						world has a whitelist enforced.
+					</p>
+				</li>
+				<li>
+					<p>
+						<code>"jam_world"</code> - Indicates whether the world is
+						currently a jam world. False for worlds that have participated
+						in a jam but were upgraded to a regular world slot.
+					</p>
+				</li>
+				<li>
+					<p>
+						<code>"jam_id"</code> - The ID of the jam this world participated
+						in. Null for worlds that weren't part of a jam.
+					</p>
+				</li>
+				<li>
+					<p>
+						<code>"whitelist_on_version_change"</code> - Indicates whether
+						the whitelist-on-version-change setting is enabled.
+					</p>
+				</li>
+				<li>
+					<p>
+						<code>"last_scraped"</code> - The unix timestamp of the world's
+						last scrape by our bot.
 					</p>
 				</li>
 			</ul>
